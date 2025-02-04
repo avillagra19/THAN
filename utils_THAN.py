12ebe17f8f9664c6b202b3b8e46f2d0b2822e4a2
Andrea <andyvillagra@gmail.com>
2025-02-04 11:15:47 +0000

    Cambie comentarios en Utils_THAN a español

--- utils_THAN.py
+++ utils_THAN.py
@@ -18,9 +18,9 @@ from NodeFeature import *
 
 from sklearn.preprocessing import OneHotEncoder
 
-# 线性变换-->降维
-# 输入：需要降维的tensor，需要输出的维度
-# 输出：返回修改维度的tensor
+# Transformación lineal -> reducción de dimensiones
+# Entrada: el tensor que se desea reducir y la dimensión de salida
+# Salida: retorna el tensor con la dimensión modificada
 def LinearChange(res, dst_feat_D):
     # print("res =", res, res.shape, dst_feat_D)
     fc = nn.Linear(res.shape[1], dst_feat_D, bias=True)
@@ -113,7 +113,7 @@ default_configure = {
     'num_heads': [8],  # Number of attention heads for node-level attention
     'hidden_units': 8,
     'dropout': 0.6,
-    'weight_decay': 0.001,  # 正则https://zhuanlan.zhihu.com/p/62393636
+    'weight_decay': 0.001,  # Regularización https://zhuanlan.zhihu.com/p/62393636
     'num_epochs': 200,
     'patience': 100
 }
@@ -154,10 +154,10 @@ def load_mydata_raw(remove_self_loop):  ####################################
     data_path = get_download_dir() + '/mydataset.mat'
     print("mydataset.mat put in:", data_path)
     data = sio.loadmat(data_path)
-    p_vs_l = data['PvsL']  # paper-field? 文章领域
-    p_vs_a = data['PvsA']  # paper-author 文章作者
-    p_vs_t = data['PvsT']  # paper-term, bag of words 文章术语-->文章中有哪些重要的词
-    p_vs_c = data['PvsC']  # paper-conference, labels come from that
+    p_vs_l = data['PvsL']  # paper-field? ¿campo de artículos? área de investigación del artículo
+    p_vs_a = data['PvsA']  # paper-author autor(es) del artículo
+    p_vs_t = data['PvsT']  # paper-term, bag of words términos del artículo -> palabras relevantes en el texto
+    p_vs_c = data['PvsC']  # paper-conference, labels come from that conferencia del artículo, de ahí provienen las etiquetas
     num = p_vs_l.shape[0]
     # test data number
     # num = 308507
@@ -181,43 +181,43 @@ def load_mydata_raw(remove_self_loop):  ####################################
     p_selected = (p_vs_c_filter.sum(1) != 0).A1.nonzero()[0]
     # od
     p_vs_l = p_vs_l[p_selected]
-    # 用户
+    # usuario
     p_vs_a = p_vs_a[p_selected]
-    # 对应项(session id or SID)的特征or属性
-    p_vs_t = p_vs_t[p_selected]  # -->在之后会改变，之前使用label赋予属性的思路不对
+    # características o atributos del elemento correspondiente (session id, SID)
+    p_vs_t = p_vs_t[p_selected]  # -->Esto cambiará más adelante; la idea previa de usar la etiqueta como atributo no era correcta
     # print("len(p_vs_t) =", p_vs_t.shape)
 
-    # 所属分类
+    # Clasificación a la que pertenece
     p_vs_c = p_vs_c[p_selected]
-    # 自己构图
+    # Construir el grafo
     hg = dgl.heterograph({
-        # 交通方式-->用户？
+        # modo de transporte -> usuario?
         ('paper', 'pa', 'author'): p_vs_a.nonzero(),
         ('author', 'ap', 'paper'): p_vs_a.transpose().nonzero(),
-        # 交通方式-->od对？
+        # modo de transporte -> par od?
         ('paper', 'pf', 'field'): p_vs_l.nonzero(),
         ('field', 'fp', 'paper'): p_vs_l.transpose().nonzero()
     })
-    # 用户数量
+    # cantidad de usuarios
     pid_num = hg.number_of_nodes('author')
-    # od数量
+    # cantidad de od
     od_num = hg.number_of_nodes('field')
     x = hg.edges('all', etype='pa')
     y = hg.edges('all', etype='pf')
-    # 空间属性
+    # Atributo espacial
     od_distance = pd.read_csv("../p38dglproject/dataset/output/beijing/train_click_od.csv", usecols=['manhattan'])
-    # 时间属性
+    # Atributo temporal
     timedata = pd.read_csv("../p38dglproject/dataset/output/beijing/time_feature.csv",
                            usecols=['req_time_hour', 'req_time_weekday', 'elapsed_time', 'minute', 'month', 'day'])
-    # 气象特征属性
+    # Atributos meteorológicos
     max_min_temp = pd.read_csv("../p38dglproject/dataset/output/beijing/time_feature.csv",
                                usecols=['max_temp', 'min_temp', 'wind'])
     weatherdata = pd.read_csv("../p38dglproject/dataset/output/beijing/time_feature.csv",
                               usecols=['weather'])
     winddata = pd.read_csv("../p38dglproject/dataset/output/beijing/time_feature.csv",
                            usecols=['wind'])
-    # plan属性
-    # -->实验证明，plan_mode_fea很重要
+    # Atributos de plan
+    # -->Se ha demostrado experimentalmente que plan_mode_fea es muy importante
     plan_mode_fea = pd.read_csv("../p38dglproject/dataset/output/beijing/plan_feature.csv",
                                 usecols=['mode_feas_1', 'mode_feas_2', 'mode_feas_3', 'mode_feas_4', 'mode_feas_5',
                                          'mode_feas_6', 'mode_feas_7', 'mode_feas_8', 'mode_feas_9', 'mode_feas_10',
@@ -238,92 +238,92 @@ def load_mydata_raw(remove_self_loop):  ####################################
                                            'energy_feas_5', 'energy_feas_6', 'energy_feas_7', 'energy_feas_8',
                                            'energy_feas_9', 'energy_feas_10', 'energy_feas_11'])
 
-    # 空间特征
+    # Características espaciales
     od_distance_feature = od_distance.values.astype(np.int)
     
-    # 时间特征
+    # Características temporales
     timefeature = timedata.values
 
-    # 气象特征
+    # Características meteorológicas
     max_min_temparrany = max_min_temp.values
     weatherdataarrany = weatherdata.values
     winddataarrany = winddata.values
 
-    # plan特征
+    # Características del plan
     plan_mode_feaarrany = plan_mode_fea.values
 
-    # 首推模式onehot编码,尽量减1，.mat中的模式都减1
+    # Se hace onehot a la modalidad “first_mode”, intentando restar 1; en .mat se restan 1 a los modos
     plan_firstmode_feaarrany = plan_firstmode_fea.values - 1
-    # 速度编码
+    # Codificación de velocidad
     plan_speed_feaarrany = plan_speed_fea.values.astype(np.int)
     plan_price_feaarrany = (plan_price_fea.values / 100).astype(np.int)
-    # 交通方式使用能源的类型
+    # Tipos de energía usados por el modo de transporte
     plan_energy_feaarrany = plan_energy_fea.values
-    # 标准化--->对于分类可能有用
+    # Estandarización -> puede ser útil para clasificación
     scaler = preprocessing.StandardScaler()
-    # 可考虑使用最大最小归一化方法，将所有值隐射到[0,1]之间
+    # Se podría usar la normalización MinMax para mapear todos los valores a [0,1]
     min_max_scaler = preprocessing.MinMaxScaler()
-    # ------>之前这里用最大最小归一化的<----------
-    # 速度的值用归一化方式的结果似乎比onehot编码的效果要好
+    # ------>Antes se usó MinMaxScaler aquí<----------
+    # Parece que normalizar los valores de velocidad da mejor resultado que onehot
     plan_speed_feaarrany = min_max_scaler.fit_transform(plan_speed_feaarrany)
-    # 用最大最小归一化效果达到，0.7422
+    # Con MinMaxScaler se logra un resultado de 0.7422
     max_min_temparrany = min_max_scaler.fit_transform(max_min_temparrany)
     winddataarrany = min_max_scaler.fit_transform(winddataarrany)
 
-    # ---->使用onehot编码总体效果提升，应该是欧式距离可加减<----- # !!! 占用空间太多
-    # ------>如果内存够，全用onehot编码都行<------#
-    # 对od的特征进行onehot编码处理
+    # ---->Usar onehot en general mejora los resultados, ya que la distancia euclidiana funciona mejor; aunque ocupa más espacio
+    # ------>Si hay suficiente memoria, se podría usar onehot para todo<------#
+    # Se realiza onehot encoding para las características de od
     enc = OneHotEncoder(sparse=False)
-    # 时间
+    # tiempo
     timefeature = enc.fit_transform(timefeature)
 
-    # 速度这项值用归一化效果更好<---已放到最大最小归一化处
+    # El valor de velocidad da mejor resultado con normalización (ya en MinMax)
     plan_firstmode_feaarrany = enc.fit_transform(plan_firstmode_feaarrany)
     plan_price_feaarrany = enc.fit_transform(plan_price_feaarrany)
     plan_energy_feaarrany = enc.fit_transform(plan_energy_feaarrany)
     od_distance_feature = enc.fit_transform(od_distance_feature)
 
-    # 气象
-    # 天气情况--->对于字符而言，还是得用onehot编码
+    # meteorología
+    # Para el clima, que son strings, seguimos usando onehot
     weatherdataarrany = enc.fit_transform(weatherdataarrany)
-    # 气温-->最大最小均值效果似乎更好
+    # Temperatura -> parece que el escalado MinMax es mejor
     max_min_temparrany = enc.fit_transform(max_min_temparrany)
-    # 风力强度 -->用原数据效果好一点-->感觉最大最小归一化不错呢？
+    # Fuerza del viento -> con datos originales va un poco mejor, pero también sirve MinMax
     winddataarrany = enc.fit_transform(winddataarrany)
 
-    # 交通方式之间的关联特征
+    # Características de asociación entre modos de transporte
 
-    # -------------------->重要特征<------------------------#
+    # -------------------->Características importantes<------------------------#
 
-    # -->用户属性，时间，空间(位置，od对)，plan中的属性：mode，speed，firstmode(首推模式)，price
+    # -->Atributos de usuario, tiempo, espacio (ubicación, par od), y atributos en plan: modo, velocidad, primer modo, precio    
     # pid_feat_feature
     # od_feat_feature
-    # 时间属性
+    # Atributos temporales
     termfeature = timefeature
 
-    # plan中的属性，分别是：mode，speed，firstmode(首推模式)，price
+    # Atributos en plan: modo, velocidad, primer modo, precio
     termfeature = np.concatenate((termfeature, plan_mode_feaarrany), axis=1)
     termfeature = np.concatenate((termfeature, plan_speed_feaarrany), axis=1)
     termfeature = np.concatenate((termfeature, plan_firstmode_feaarrany), axis=1)
     termfeature = np.concatenate((termfeature, plan_price_feaarrany), axis=1)
 
-    # 交通方式使用的能量是不同的--->使用能量种类不同
+    # Distintos modos de transporte usan distintos tipos de energía
     termfeature = np.concatenate((termfeature, plan_energy_feaarrany), axis=1)
-    # -->距离对交通方式的选择具有一定的影响，比如小于10km时，更希望步行或乘坐自行车
+    # -->La distancia influye en la elección del modo de transporte; p. ej., menos de 10km se prefiere caminar o bicicleta
     termfeature = np.concatenate((termfeature, od_distance_feature), axis=1)
-    # 气象特征-->全部加入，性能就提升了，不要单独放入(单独放入，性能似乎下降！！！)
+    # Atributos meteorológicos -> al incorporarlos todos, mejora el rendimiento; si se ponen aislados, parece empeorar
     
-    # -->天气状况
+    # -->Estado del clima
     termfeature = np.concatenate((termfeature, weatherdataarrany), axis=1)
-    # -->最高，最低温度
+    # -->Temperatura máxima y mínima
     termfeature = np.concatenate((termfeature, max_min_temparrany), axis=1)
-    # -->风力强度-->0.7058
+    # -->Intensidad del viento -> 0.7058
     termfeature = np.concatenate((termfeature, winddataarrany), axis=1)
 
-    # --------->保存当前生成的原始特征<--------#
-    # 原始属性--》主要是用户属性p0-p65
+    # --------->Guardar las características originales generadas actualmente<--------#
+    # Atributos originales -> principalmente atributos de usuario p0-p65
     origin_feature = pd.read_csv("../p38dglproject/dataset/output/beijing/beijing_nonoise.csv")
-    # 这个属性，不包含sid、req_time、o、d、plan_time、plans、click_time、click_mode、city_flag_o
+    # Este atributo no incluye sid, req_time, o, d, plan_time, plans, click_time, click_mode, city_flag_o
     del origin_feature['sid']
     # del origin_feature['pid']
     del origin_feature['req_time']
@@ -332,7 +332,7 @@ def load_mydata_raw(remove_self_loop):  ####################################
     del origin_feature['plan_time']
     del origin_feature['plans']
     del origin_feature['click_time']
-    # 这几项按道理是需要的
+    # Eliminar filas duplicadas según pid
     del origin_feature['o_lng']
     del origin_feature['o_lat']
     del origin_feature['d_lng']
@@ -340,71 +340,68 @@ def load_mydata_raw(remove_self_loop):  ####################################
     del origin_feature['click_mode']
     del origin_feature['city_flag_o']
 
-    # 按pid删除重复行
+    # Eliminar filas duplicadas según pid
     origin_feature = origin_feature.drop_duplicates(subset=['pid'], keep='first', inplace=False)
     del origin_feature['pid']
 
-    # 本身就是onehot编码
+    # Ya está en onehot
     origin_feature_new = origin_feature.values
     origin_feature_new = origin_feature_new[:pid_num]
 
     print("origin_feature_new =", origin_feature_new, origin_feature_new.shape)
 
-    # 空间属性
-    # o和d-->对d,o, pid, time的特征
-    # od属性
+    # Atributos espaciales (o y d) -> características de d, o, pid, time
+
     o_d_lng_lat = pd.read_csv("../p38dglproject/dataset/output/beijing/beijing_nonoise.csv",
                               usecols=['o_lng', 'o_lat', 'd_lng', 'd_lat', 'o', 'd'])
     o_d_lng_lat['od'] = o_d_lng_lat['o'] + o_d_lng_lat['d']
-    # 按od删除重复行
+    # Eliminar filas duplicadas según od
     o_d_lng_lat = o_d_lng_lat.drop_duplicates(subset=['od'], keep='first', inplace=False)
     del o_d_lng_lat['od']
     del o_d_lng_lat['o']
     del o_d_lng_lat['d']
     # del o_d_lng_lat['pid_ratio']
     print("o_d_lng_lat =", o_d_lng_lat[:od_num])
-    # d属性
-    # 转换为二维数组
+    # Se convierte en un arreglo bidimensional
     o_d_lng_lat_feature = o_d_lng_lat.values
-    # 利用onehot编码可以增强效果
+    # El onehot encoding puede mejorar el efecto 
     o_d_lng_lat_feature = enc.fit_transform(o_d_lng_lat_feature)
     o_d_lng_lat_feature = o_d_lng_lat_feature[:od_num]
-    # o属性
+    # Atributos de o
     o_lng_lat = pd.read_csv("../p38dglproject/dataset/output/beijing/beijing_nonoise.csv",
                               usecols=['o_lng', 'o_lat', 'o', 'd'])
     o_lng_lat['od'] = o_lng_lat['o'] + o_lng_lat['d']
 
-    # 节点不一定用完
+    # No necesariamente se usan todos los nodos
     o_lng_lat = o_lng_lat[:num]
     print("o_lng_lat =", o_lng_lat)
-    # 按o删除重复行
+    # Eliminar duplicados según o
     o_lng_lat = o_lng_lat.drop_duplicates(subset=['o'], keep='first', inplace=False)
     del o_lng_lat['o']
     del o_lng_lat['d']
     del o_lng_lat['od']
 
-    # d属性
-    # 转换为二维数组
+    # Se convierte en un arreglo bidimensional
     o_lng_lat_feature = o_lng_lat.values
-    # 利用onehot编码可以增强效果
+    # El onehot encoding mejora el efecto
     o_lng_lat_feature = enc.fit_transform(o_lng_lat_feature)
 
-    # d属性
+    # Atributos de d
     d_lng_lat = pd.read_csv("../p38dglproject/dataset/output/beijing/beijing_nonoise.csv",
                               usecols=['d_lng', 'd_lat', 'o', 'd'])
     d_lng_lat['od'] = d_lng_lat['o'] + d_lng_lat['d']
     d_lng_lat = d_lng_lat[:num]
-    # 按d删除重复行
+    # Eliminar duplicados según d
     d_lng_lat = d_lng_lat.drop_duplicates(subset=['d'], keep='first', inplace=False)
     del d_lng_lat['o']
     del d_lng_lat['d']
     del d_lng_lat['od']
 
-    # 转换为二维数组
+    # Se convierte en un arreglo bidimensional
     d_lng_lat_feature = d_lng_lat.values
-    # 利用onehot编码可以增强效果
+    # El onehot encoding puede mejorar el efecto
     d_lng_lat_feature = enc.fit_transform(d_lng_lat_feature)
-    # 相当于提取交通方式的特征
+    # Equivale a extraer las características del modo de transporte
     features = torch.FloatTensor(termfeature[:num])
     print("features =", features, features.shape)
     print("len(features) =", len(features))
@@ -412,17 +409,17 @@ def load_mydata_raw(remove_self_loop):  ####################################
     od_feature = torch.FloatTensor(o_d_lng_lat_feature)
     o_feature = torch.FloatTensor(o_lng_lat_feature)
     d_feature = torch.FloatTensor(d_lng_lat_feature)
-    # 不可取，在这里降维，其中的参数无法学习，会报错
-    # 将od_feature降维到o_feature+d_feature的维度
+    # No es conveniente reducir dimensiones aquí, los parámetros no pueden aprenderse y producirá error
+    # Tratar de reducir od_feature a la dimensión de o_feature + d_feature
     pc_p, pc_c = p_vs_c.nonzero()
     labels = np.zeros(len(p_selected), dtype=np.int64)
     for conf_id, label_id in zip(conf_ids, label_ids):
         labels[pc_p[pc_c == conf_id]] = label_id
     labels = torch.LongTensor(labels)
-    # 赋予标签
-    # 分类数
+    # Asignar etiquetas
+    # Número de clases
     num_classes = 11
-    # 掩码？-->用于拆分训练集、验证集和测试集
+    # ¿Máscara? -> Para separar entrenamiento, validación y test
     float_mask = np.zeros(len(pc_p))
     for conf_id in conf_ids:
         pc_c_mask = (pc_c == conf_id)
@@ -432,13 +429,13 @@ def load_mydata_raw(remove_self_loop):  ####################################
     val_idx = np.where((float_mask > 0.7) & (float_mask <= 0.8))[0]
     test_idx = np.where(float_mask > 0.8)[0]
 
-    # 这里就挺明确了，以SID为主
+    # Aquí está claro: se basa principalmente en SID
     num_nodes = hg.number_of_nodes('paper')
     train_mask = get_binary_mask(num_nodes, train_idx)
     val_mask = get_binary_mask(num_nodes, val_idx)
     test_mask = get_binary_mask(num_nodes, test_idx)
 
-    # 将测试集分为4部分
+    # Dividir el conjunto de prueba en 4 partes
     a = np.array_split(test_idx, 4)
     test_idx_0 = a[0]
     test_idx_1 = a[1]
@@ -448,13 +445,13 @@ def load_mydata_raw(remove_self_loop):  ####################################
     test_mask_1 = get_binary_mask(num_nodes, test_idx_1)
     test_mask_2 = get_binary_mask(num_nodes, test_idx_2)
     test_mask_3 = get_binary_mask(num_nodes, test_idx_3)
-    # ----->可考虑加上频率，作为权重<--------?是否会更好??有待考证
+    # ----->Se podría considerar añadir frecuencia como peso -> ¿Mejoraría? Falta verificar
     o_d_count = pd.read_csv(path + who + '/o_d_count.csv')
     del o_d_count['Unnamed: 0']
     o_d_count = o_d_count[:num]
     # pd.DataFrame to numpy
     o_d_count = o_d_count.values
-    # 从二维降为一维
+    # Se aplana de 2D a 1D
     o_d_count = o_d_count.reshape(-1, 1)
     o_list, d_list = extract_od_list()
     o_list = o_list[:num]
@@ -480,7 +477,7 @@ def load_data(dataset, remove_self_loop=False):
 class EarlyStopping(object):
     def __init__(self, patience=10):
         dt = datetime.datetime.now()
-        # -->如果同时训练的话，还是得把它放开，分时训练
+        # -->Si se entrena simultáneamente, habría que habilitarlo para entrenar por turnos
         self.filename = 'early_stop_{}_{:02d}-{:02d}-{:02d}.pth'.format(
             dt.date(), dt.hour, dt.minute, dt.second)
         # self.filename = 'early_stop.pth'
@@ -494,7 +491,7 @@ class EarlyStopping(object):
         if self.best_loss is None:
             self.best_acc = acc
             self.best_loss = loss
-            # 上一次的模型接着导入训练
+            # Cargar el último modelo y continuar entrenando
             # self.load_checkpoint(model)
             self.save_checkpoint(model)
         elif (loss > self.best_loss) and (acc < self.best_acc):
